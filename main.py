c5b2f439b73f2f027d9f32862cc5640b8383a853
parinzee <parinzee@gmail.com>
2020-08-01 12:51:21 +0700

    Pending Backend of AP calculation

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, url_for, flash, redirect, request
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'dc2ff804898bac95f18c1d4f13cb6d127bbb48a86b99632cbe06931e3ec46776'
+app.config[
+    "SECRET_KEY"
+] = "dc2ff804898bac95f18c1d4f13cb6d127bbb48a86b99632cbe06931e3ec46776"
 
 
 def calculation(grade):
@@ -89,23 +91,23 @@ def calculation2(grade):
 
 @app.route("/")
 def home_page():
-    return render_template('home.html')
+    return render_template("home.html")
 
 
-@app.route("/normal", methods=['GET', 'POST'])
+@app.route("/normal", methods=["GET", "POST"])
 def normal_page():
-    if request.method == 'POST':
-        core1 = request.form['core1']
-        core2 = request.form['core2']
-        core3 = request.form['core3']
-        core4 = request.form['core4']
+    if request.method == "POST":
+        core1 = request.form["core1"]
+        core2 = request.form["core2"]
+        core3 = request.form["core3"]
+        core4 = request.form["core4"]
 
-        elect1 = request.form['elective1']
-        elect2 = request.form['elective2']
-        elect3 = request.form['elective3']
-        elect4 = request.form['elective4']
-        elect5 = request.form['elective5']
-        elect6 = request.form['elective6']
+        elect1 = request.form["elective1"]
+        elect2 = request.form["elective2"]
+        elect3 = request.form["elective3"]
+        elect4 = request.form["elective4"]
+        elect5 = request.form["elective5"]
+        elect6 = request.form["elective6"]
 
         calculation(core1)
         core1 = calculation.grade
@@ -143,25 +145,26 @@ def normal_page():
 
         final_grade = round(final_grade, 2)
 
-        return render_template('normal.html', title='Normal', final_grade=final_grade)
-    
+        return render_template("normal.html", title="Normal", final_grade=final_grade)
+
     else:
-        return render_template('normal.html', title='Normal')
+        return render_template("normal.html", title="Normal")
 
-@app.route("/honors", methods=['GET', 'POST'])
+
+@app.route("/honors", methods=["GET", "POST"])
 def beta_page():
-    if request.method == 'POST':
-        core1 = request.form['core1']
-        core2 = request.form['core2']
-        core3 = request.form['core3']
-        core4 = request.form['core4']
+    if request.method == "POST":
+        core1 = request.form["core1"]
+        core2 = request.form["core2"]
+        core3 = request.form["core3"]
+        core4 = request.form["core4"]
 
-        elect1 = request.form['elective1']
-        elect2 = request.form['elective2']
-        elect3 = request.form['elective3']
-        elect4 = request.form['elective4']
-        elect5 = request.form['elective5']
-        elect6 = request.form['elective6']
+        elect1 = request.form["elective1"]
+        elect2 = request.form["elective2"]
+        elect3 = request.form["elective3"]
+        elect4 = request.form["elective4"]
+        elect5 = request.form["elective5"]
+        elect6 = request.form["elective6"]
 
         calculation2(core1)
         core1 = calculation2.grade
@@ -199,12 +202,14 @@ def beta_page():
 
         final_grade = round(final_grade, 2)
 
-        return render_template('beta.html', title='Beta', final_grade=final_grade)
-        
+        return render_template("beta.html", title="Honors", final_grade=final_grade)
+
     else:
-        
-        return render_template('beta.html', title='Honors')
+
+        return render_template("beta.html", title="Honors")
+
 
 @app.route("/credits")
 def credits():
-    return render_template('credits.html', title='Credits!')
+    return render_template("credits.html", title="Credits!")
+
