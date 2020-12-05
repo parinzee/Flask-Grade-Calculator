c1d5593b7c937b4f045e687a84d641d6c7373b57
Parinthapat Pengpun <parinzee@protonmail.com>
2020-12-05 14:03:36 +0700

    Integrated JS

---
+++ static/elementary.js
@@ -0,0 +1,29 @@
+/*jshint esversion: 6*/
+function CalculateGrade() {
+    let grade = [];
+
+    for (let i = 0; i < 5; i++) {
+        grade[i] = document.getElementsByName("sub")[i].value;
+    }
+
+    let Final = 0;
+
+    for (let i = 0; i < 5; i++) {
+        
+
+        Final += parseFloat(grade[i]);
+    }
+    console.log(Final);
+
+    Final = Final / 5;
+
+    if (isNaN(Final)) {
+        document.getElementById("result").innerHTML = "<b style='font-size: 20px;'> " + "Calculating..." + " </b>";
+    }
+
+    else {
+        document.getElementById("result").innerHTML = "<b style='font-size: 40px;'> " + Final + " </b>";
+    }
+
+    
+}
