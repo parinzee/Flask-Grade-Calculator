c3ebe8036fefb1d4ac201fd53e403d508c3f7736
Parinthapat Pengpun <parinzee@protonmail.com>
2020-12-06 10:16:11 +0700

    Prettier Format

--- static/standard.js
+++ static/standard.js
@@ -4,82 +4,59 @@ function Calculate_S(grade) {
 
     if (grade >= 93) {
         return 4.0;
-    }
-
-    else if (grade >= 90) {
+    } else if (grade >= 90) {
         return 3.7;
-    }
-
-    else if (grade >= 87) {
+    } else if (grade >= 87) {
         return 3.3;
-    }
-
-    else if (grade >= 83) {
+    } else if (grade >= 83) {
         return 3.0;
-    }
-
-    else if (grade >= 80) {
+    } else if (grade >= 80) {
         return 2.7;
-    }
-
-    else if (grade >= 77) {
+    } else if (grade >= 77) {
         return 2.3;
-    }
-
-    else if (grade >= 73) {
+    } else if (grade >= 73) {
         return 2.0;
-    }
-
-    else if (grade >= 70) {
+    } else if (grade >= 70) {
         return 1.7;
-    }
-
-    else if (grade >= 67) {
+    } else if (grade >= 67) {
         return 1.3;
-    }
-
-    else if (grade >= 63) {
+    } else if (grade >= 63) {
         return 1.0;
-    }
-
-    else if (grade >= 60) {
+    } else if (grade >= 60) {
         return 0.7;
-    }
-
-    else {
+    } else {
         return 0;
     }
 }
 
 function CalculateGrade() {
     // Core Subjects Length
-    let core_subs_len = document.getElementsByName('core').length;
+    let core_subs_len = document.getElementsByName("core").length;
 
     // Array for looping through
     let core_subs = [];
 
     for (let i = 0; i < core_subs_len; i++) {
-        core_subs[i] = document.getElementsByName('core')[i].value; 
+        core_subs[i] = document.getElementsByName("core")[i].value;
     }
 
     //Elective_Subjects Array
-    let elective_subs_len = document.getElementsByName('elective').length;
+    let elective_subs_len = document.getElementsByName("elective").length;
     let elective_subs = [];
 
     for (let i = 0; i < elective_subs_len; i++) {
-        elective_subs[i] = document.getElementsByName('elective')[i].value; 
+        elective_subs[i] = document.getElementsByName("elective")[i].value;
     }
-
 
     let core_sum = 0;
     let elective_sum = 0;
 
     // Looping Through Core
-    for (let i = 0; i < 4; i ++) {
+    for (let i = 0; i < 4; i++) {
         core_sum += Calculate_S(core_subs[i]);
     }
 
-    for (let i = 0; i < 6; i ++) {
+    for (let i = 0; i < 6; i++) {
         elective_sum += Calculate_S(elective_subs[i]);
     }
 
@@ -87,6 +64,7 @@ function CalculateGrade() {
     let Final_Elect = elective_sum * 0.25;
     let Final = Final_Core + Final_Elect;
     let Final_Grade = Final / 3.5;
-    
-    document.getElementById("result").innerHTML = "<b style='font-size: 40px;'> " + Final_Grade.toFixed(2) + " </b>";
+
+    document.getElementById("result").innerHTML =
+        "<b style='font-size: 40px;'> " + Final_Grade.toFixed(2) + " </b>";
 }
